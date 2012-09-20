fa0ec8da9374c558af161c202e7ffc85160d0ac2
galvarez <guillaume.alvarez@gmail.com>
2012-09-20 21:15:17 +0200

    must have volatile variable to be read from GUI thread

--- core/penran/zombies/core/Place.java
+++ core/penran/zombies/core/Place.java
@@ -10,52 +10,52 @@ import java.util.List;
  */
 public final class Place implements GameObject {
 
-	public final String name;
+  public final String name;
 
-	public final Coordinates coordinates;
+  public final Coordinates coordinates;
 
-	// TODO optimize by using a common structure for all links
-	private final List<Link> links = new ArrayList<Link>();
+  // TODO optimize by using a common structure for all links
+  private final List<Link> links = new ArrayList<Link>();
 
-	/** Percentage of zombies here. */
-	private double zombies = 0;
+  /** Percentage of zombies here. Must be volatile to be read from UI thread. */
+  private volatile double zombies = 0;
 
-	public Place(String name, Coordinates coordinates) {
-		this.name = name;
-		this.coordinates = coordinates;
-	}
+  public Place(String name, Coordinates coordinates) {
+    this.name = name;
+    this.coordinates = coordinates;
+  }
 
-	/**
-	 * Register a link in this place.
-	 */
-	/* package */void addLink(Link link) {
-		links.add(link);
-	}
+  /**
+   * Register a link in this place.
+   */
+  /* package */void addLink(Link link) {
+    links.add(link);
+  }
 
-	public boolean hasZombies() {
-		return zombies > 0;
-	}
+  public boolean hasZombies() {
+    return zombies > 0;
+  }
 
-	public double getZombies() {
-		return zombies;
-	}
+  public double getZombies() {
+    return zombies;
+  }
 
-	/**
-	 * Add a percentage to current zombie presence. The percentage can be
-	 * negative to avoid
-	 */
-	void addZombies(double percent) {
-		if (percent > 0)
-			zombies = Math.max(100.0, zombies + percent);
-		else if (percent < 0)
-			zombies = Math.min(0.0, zombies + percent);
+  /**
+   * Add a percentage to current zombie presence. The percentage can be negative
+   * to avoid
+   */
+  void addZombies(double percent) {
+    if (percent > 0)
+      zombies = Math.max(100.0, zombies + percent);
+    else if (percent < 0)
+      zombies = Math.min(0.0, zombies + percent);
 
-	}
+  }
 
-	@Override
-	public void tick() {
-		// TODO Auto-generated method stub
+  @Override
+  public void tick() {
+    // TODO Auto-generated method stub
 
-	}
+  }
 
 }

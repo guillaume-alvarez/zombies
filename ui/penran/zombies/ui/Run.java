7170c2bce674459ddf35844b945d8bb00548febf
galvarez <guillaume.alvarez@gmail.com>
2014-06-11 22:51:25 +0200

    comments (can I haz some lambdas???)

--- ui/penran/zombies/ui/Run.java
+++ ui/penran/zombies/ui/Run.java
@@ -18,33 +18,31 @@ public class Run extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
+    // create display scene
     BorderPane layout = new BorderPane();
     Scene scene = new Scene(layout);
     scene.getStylesheets().add(this.getClass().getResource("stylesheet.css").toExternalForm());
     stage.setScene(scene);
     stage.show();
-    
-    // load game data
-    Land land = new Land(Level.load(new File("etc/level.test")), 800, 350, 20, 20);
 
-    // launch display with technical information (mouse, fps...)
+    // load game data and populate display
+    Land land = new Land(Level.load(new File("etc/level.test")), 800, 350, 20, 20);
     layout.setCenter(land);
-    layout.setBottom(techInfo(land));
+
+    // publish some technical information (mouse, fps...)
+    final Label tech = new Label("unknown");
+    // TODO please lambdas!!!
+    land.setOnMouseMoved(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+        tech.setText("Longitude: " + event.getSceneX() + ", latitude: " + event.getSceneY());
+      }
+    });
+    layout.setBottom(tech);
 
     // start game
     land.beginGameLoop();
 
-  }
-
-  private Node techInfo(Land land) {
-    final Label mouse = new Label("unknown");
-    land.setOnMouseMoved(new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent event) {
-        mouse.setText("Longitude: " + event.getSceneX() + ", latitude: " + event.getSceneY());
-      }
-    });
-    return mouse;
   }
 
   @Override

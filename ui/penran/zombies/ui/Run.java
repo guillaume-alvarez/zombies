937fc52e43d1e9a0f55656895616ae52ceae1485
galvarez <guillaume.alvarez@gmail.com>
2014-06-11 22:49:58 +0200

    correct layout wrt to JavaFX best practices (let the window guess its size from components inside the Scene)

--- ui/penran/zombies/ui/Run.java
+++ ui/penran/zombies/ui/Run.java
@@ -8,7 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Run extends Application {
@@ -18,20 +18,22 @@ public class Run extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    // load game data
-    Land land = new Land(Level.load(new File("etc/level.test")), 800, 350, 20, 20);
-
-    // launch display with mouse coordinates
-    VBox layout = new VBox();
-    layout.getChildren().setAll(land, techInfo(land));
+    BorderPane layout = new BorderPane();
     Scene scene = new Scene(layout);
     scene.getStylesheets().add(this.getClass().getResource("stylesheet.css").toExternalForm());
     stage.setScene(scene);
+    stage.show();
+    
+    // load game data
+    Land land = new Land(Level.load(new File("etc/level.test")), 800, 350, 20, 20);
+
+    // launch display with technical information (mouse, fps...)
+    layout.setCenter(land);
+    layout.setBottom(techInfo(land));
 
     // start game
     land.beginGameLoop();
 
-    stage.show();
   }
 
   private Node techInfo(Land land) {
